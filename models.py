71c1b33582121afa9503b66025dba98f673a4c7d
Poucag <poucagado123@gmail.com>
2026-02-15 12:09:02 -0300

    models.py

---
+++ models.py
@@ -0,0 +1,6 @@
+NIM_MODELS = {
+    "kimi-v2.5": "moonshotai/kimi-k2-instruct",
+    "glm-4.7": "zhipuai/glm-4.7",
+    "glm-5": "zhipuai/glm-5",
+    "deepseek-v3.2": "deepseek-ai/deepseek-v3.2"
+}
